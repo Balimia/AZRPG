91c4d3a1c3ba55790d77d1ef0953e1b1dac6065f
Balimia <adrian.mauroschat@gmail.com>
2021-10-05 00:53:00 +0200

    add command filtering system

---
+++ game/utils/filterMessage.js
@@ -0,0 +1,33 @@
+const { PREFIX } = require('../../admin/config.json');
+const collection = require('../utils/commands');
+const { isPlayerSpam, isPlayerBusy, setSpam } = require('./sets');
+
+const filterMessage = async (message) => {
+	const [commandName, args] = sanitize(message);
+	const command = findCommand(commandName);
+	const reason = notifyError(message, command);
+
+	if (reason) return console.log(`Send embed: ${reason}`); // TODO REPLACE BY EMBED
+
+	setSpam(message);
+	findDBPlayer(message, command, args);
+};
+
+const sanitize = (message) => {
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+	const commandName = args.shift();
+	return [commandName, args];
+};
+
+const findCommand = (name) => collection.find((cmd) => cmd.alias?.includes(name));
+
+const notifyError = (message, command) => {
+	if (!command) return 'help';
+	if (isPlayerSpam(message)) return 'spam';
+	if (command.busy && isPlayerBusy(message)) return 'busy';
+	return null;
+};
+
+const findDBPlayer = (a, b, c) => console.log('searching DB'); // TODO FOLLOW THE THREAD
+
+module.exports = filterMessage;
