f1fe6f0dd2dcd6370e067e6a54ebe3ca8cc76833
Balimia <adrian.mauroschat@gmail.com>
2021-10-06 18:33:53 +0200

    update clientOption to reflect library change

--- admin/services/client.js
+++ admin/services/client.js
@@ -3,12 +3,20 @@ const { Client } = require('discord.js');
 const client = new Client({
 	presence: {
 		status: 'online',
-		activity: {
-			name: 'AZ RPG',
-			type: 'PLAYING',
-		},
+		activities: [
+			{
+				name: 'AZ RPG',
+				type: 'PLAYING',
+			},
+		],
 	},
-	intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS'],
+	intents: [
+		'GUILDS',
+		'GUILD_MESSAGES',
+		'GUILD_MESSAGE_REACTIONS',
+		'DIRECT_MESSAGES',
+		'DIRECT_MESSAGE_REACTIONS',
+	],
 });
 
 module.exports = client;
