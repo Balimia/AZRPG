8cf21e614458ddb9b2487a4d1bed7ea366de7339
Balimia <adrian.mauroschat@gmail.com>
2021-10-08 06:11:41 +0200

    implement barrel imports

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const client = require('./admin/services/client');
-const db = require('./admin/services/db');
-
-const { write } = require('./admin/logs/logger');
+const { client, db } = require('./admin');
 const { readdir } = require('fs/promises');
+const { TOKEN } = require('./config.json');
+const { logger } = require('./game/utils');
 
-const { TOKEN } = require('./admin/config.json');
-// const startDB = require('./zDevDump/startDB');
-
-readdir('./events/')
+readdir('./game/events/')
 	.then((files) =>
 		files
 			.filter((f) => f.endsWith('.js'))
-			.forEach((f) => client.on(f.split('.')[0], require(`./events/${f}`)))
+			.forEach((f) => client.on(f.split('.')[0], require(`./game/events/${f}`)))
 	)
-	.catch((err) => write(`Error while handling events:\n${err?.stack}`));
+	.catch((err) => logger.write(`Error while handling events:\n${err?.stack}`));
 
+// const startDB = require('./zDevDump/startDB');
 db.init()
 	// .then(() => startDB())
 	.then(() => client.login(TOKEN))
-	.catch((err) => write(`Error while starting up.\n${err?.stack}`));
+	.catch((err) => logger.write(`Error while starting up.\n${err?.stack}`));
