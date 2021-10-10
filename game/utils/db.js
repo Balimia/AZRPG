b92672e72324f34551c49fa61bbc221a8fccc141
Balimia <adrian.mauroschat@gmail.com>
2021-10-10 07:12:44 +0200

    revert a previous file split

--- game/utils/db.js
+++ game/utils/db.js
@@ -1,10 +1,12 @@
 const logger = require('./logger');
-const helpers = require('./db.helpers');
+const { db } = require('../../admin/index');
+const mysql = require('./var/mysql');
+const { COMBAT_STATS, SKILLS } = require('../lib/globals');
 
 const getPlayer = async (message) => {
 	try {
-		const player = await helpers.searchPlayer(message);
-		return player ? [false, player] : [true, await helpers.createPlayer(message)];
+		const player = await searchPlayer(message);
+		return player ? [false, player] : [true, await createPlayer(message)];
 	} catch (error) {
 		logger.write(error?.stack);
 	}
@@ -12,24 +14,55 @@ const getPlayer = async (message) => {
 
 const getCooldown = async (message, name) => {
 	try {
-		const cooldown = await helpers.searchCooldown(message.author.id, name);
-		if (!cooldown) await helpers.createCooldown(message.author.id, name);
+		const cooldown = await searchCooldown(message.author.id, name);
+		if (!cooldown) await createCooldown(message.author.id, name);
 		return cooldown || null;
 	} catch (error) {
 		logger.write(error?.stack);
 	}
 };
 
-const executeCommand = async (message, player, args, command) => {
-	try {
-		if (isMissingArgs(command, args))
-			return await sendMsg(message, embedMissingArg(message, command));
-		if (command.name === 'Help') args = isHelpCommand(args);
-		return await command.execute(message, player, args);
-	} catch (error) {
-		return console.error(error); // DEBUGGING ONLY
-	}
-};
+const searchPlayer = (message) =>
+	new Promise((res, rej) => {
+		db.execute(mysql.getProfile, [message.author.id])
+			.then((r) => res(r[0]))
+			.catch((err) => rej(err));
+	});
+
+const createPlayer = (message) =>
+	new Promise((res, rej) => {
+		db.execute(mysql.createUser, [message.author.id, message.author.username])
+			.then(() => setUserStats(message))
+			.then(() => setUserCombat(message))
+			.then(() => setUserSkills(message))
+			.then(() => searchPlayer(message))
+			.then((r) => res(r))
+			.catch((err) => rej(err));
+	});
+
+const searchCooldown = (id, name) =>
+	new Promise((res, rej) => {
+		db.execute(mysql.getSpecificCooldown, [id, name])
+			.then((r) => res(r[0]))
+			.catch((err) => rej(err));
+	});
+
+const createCooldown = (id, name) =>
+	new Promise((res, rej) => {
+		db.execute(mysql.createUserCooldown, [id, name])
+			.then((r) => res(r))
+			.catch((err) => rej(err));
+	});
+
+const setUserStats = (message) => db.execute(mysql.createUserStats, [message.author.id]);
+
+const setUserCombat = (message) =>
+	COMBAT_STATS.forEach((e, index) =>
+		db.execute(mysql.createUserCombat, [message.author.id, index + 1, e.default, e.default])
+	);
+
+const setUserSkills = (message) =>
+	SKILLS.forEach((s, index) => db.execute(mysql.createUserSkills, [message.author.id, index + 1]));
 
 module.exports = {
 	getPlayer,
