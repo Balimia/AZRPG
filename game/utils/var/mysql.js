b7654ff2e2f4ed1b06b6795a4fff5173ce0353d2
Balimia <adrian.mauroschat@gmail.com>
2021-10-12 17:39:24 +0200

    change db column name formatting to lowercase

--- game/utils/var/mysql.js
+++ game/utils/var/mysql.js
@@ -1,15 +1,16 @@
-const getIdFromDiscordId = 'SELECT id FROM User WHERE DiscordId = ?';
-const getIdFromCooldown = 'SELECT id FROM Cooldown WHERE Name = ?';
-// const getUserById = 'SELECT * FROM User WHERE DiscordId = ?';
+const getIdFromDiscordId = 'SELECT id FROM User WHERE discord_id = ?';
+const getIdFromCooldown = 'SELECT id FROM Cooldown WHERE name = ?';
+// const getUserById = 'SELECT * FROM User WHERE discord_id = ?';
 const getProfile =
-	'SELECT DiscordId, Username, Membership, Banned, CurrentCity, HighestCity, Level, Experience FROM User JOIN UserStats ON User.id = id_User WHERE DiscordId = ?';
+	'SELECT discord_id, username, membership, banned, current_city, highest_city, level, experience FROM User JOIN UserStats ON User.id = id_User WHERE discord_id = ?';
 const getSpecificCooldown = `SELECT * FROM UserCooldown WHERE id_User = (${getIdFromDiscordId}) AND id_Cooldown = (${getIdFromCooldown})`;
 
-const createUser = 'INSERT INTO User (DiscordId, Username) VALUES (?, ?)';
+const setUserCooldown = `INSERT INTO UserCooldown (id_User, id_Cooldown, cooldown) VALUES ((${getIdFromDiscordId}), (${getIdFromCooldown}), now()) ON DUPLICATE KEY UPDATE cooldown = now()`;
+
+const createUser = 'INSERT INTO User (discord_id, username) VALUES (?, ?)';
 const createUserStats = `INSERT INTO UserStats (id_User) VALUES ((${getIdFromDiscordId}))`;
-const createUserCombat = `INSERT INTO UserCombat (id_User, id_Combat, Current, Max) VALUES ((${getIdFromDiscordId}), ?, ?, ?)`;
+const createUserCombat = `INSERT INTO UserCombat (id_User, id_Combat, current, max) VALUES ((${getIdFromDiscordId}), ?, ?, ?)`;
 const createUserSkills = `INSERT INTO UserSkills (id_User, id_Skill) VALUES ((${getIdFromDiscordId}), ?)`;
-const createUserCooldown = `INSERT INTO UserCooldown (id_User, id_Cooldown, Cooldown) VALUES ((${getIdFromDiscordId}), (${getIdFromCooldown}), now())`;
 
 module.exports = {
 	createUser,
@@ -18,5 +19,5 @@ module.exports = {
 	createUserSkills,
 	getProfile,
 	getSpecificCooldown,
-	createUserCooldown,
+	setUserCooldown,
 };
